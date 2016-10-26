4400d993e294a824e421877bc92b296db6aee3dd
ajay-singh <lanetteam.ajay@gmail.com>
2016-10-26 20:38:16 +0530

    commit for complete1

---
+++ app/src/main/java/com/guam/museumentry/global/DatabaseUtils.java
@@ -0,0 +1,8 @@
+package com.guam.museumentry.global;
+
+/**
+ * Created by lcom75 on 26/10/16.
+ */
+
+public class DatabaseUtils {
+}
---
+++ app/src/main/java/com/guam/museumentry/service/FetchDetails.java
@@ -0,0 +1,8 @@
+package com.guam.museumentry.service;
+
+/**
+ * Created by lcom75 on 26/10/16.
+ */
+
+public class FetchDetails {
+}
